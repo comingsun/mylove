63632737f66b5dadb8d748fba365b99ba2923e3b
testgit <1183626725@qq.com>
2019-04-12 13:35:21 +0800

    登陆

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -3,15 +3,27 @@
 namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
-use Illuminate\Support\Facades\Request;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class LoginController extends Controller
 {
 
     public function doLogin()
     {
-        echo Request::handle();exit;
         $input = Request::all();
-        return $input;
+        $username = $input['Username'];
+        $password = $input['Password'];
+        $users = DB::table('user')->where([['username','=',$username],['password','=',$password]])->first();
+        if($users->id){
+            session(['isLogin' => '1']);
+        }else{
+            session(['isLogin' => '-1']);
+        }
+        return redirect('user/index');
+    }
+
+    public function testUrl(){
+
     }
 }
